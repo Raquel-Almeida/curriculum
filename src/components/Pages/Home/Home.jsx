d361af6d3681c76a42ffd9016e66eddacee4504e
Raquel Almeida <87998797+Raquel-Almeida@users.noreply.github.com>
2022-03-05 00:36:03 +0000

    Home Page Updated

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -5,9 +5,15 @@ import "./Home.scss";
 
 export default function Home() {
   return (
-    <div className="home">
-      <img className="profile-photo" src={profilePhoto} alt="Profile"></img>
-      <p>About me</p>
-    </div>
+    <section className="home">
+      <div className="profile">
+        <img className="profile-photo" src={profilePhoto} alt="Profile"></img>
+        <div className="about-me-container"> 
+          <p className="job">Front-End Web Developer</p>
+          <p className="name">Raquel Almeida</p>
+          <p className="about-me">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+    </section>
   );
 }
