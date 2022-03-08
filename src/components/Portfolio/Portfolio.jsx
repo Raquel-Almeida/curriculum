4d0151411f5f9084322545dc654ac0335e91c8a0
Raquel Almeida <87998797+Raquel-Almeida@users.noreply.github.com>
2022-03-08 19:24:39 +0000

    Portfolio Updated

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,19 +1,43 @@
 import React from "react";
-import Card from "../UI/Card/Card";
-import Button from "../UI/Button/Button";
 
 import styles from "./Portfolio.module.scss";
-
 import FoodDeliveryDevices from "../../assets/FoodDeliveryDevices.jpg";
 import CurriculumDevices from "../../assets/CurriculumDevices.jpg";
 
 export default function Portfolio() {
   return (
-    <section className={styles.portfolio} id="portfolio">
-      <div className={styles.projects_container}>
+    <section className={styles.portfolio_container} id="portfolio">
+      <div className={styles.portfolio}>
         <h2>03. Things I've Built</h2>
         <div className={styles.projects}>
-          <Card>
+          <div className={styles.project}>
+            <img src={FoodDeliveryDevices} alt="" />
+            <h3>Food Delivery App</h3>
+            <p className={styles.description}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.{" "}
+            </p>
+          </div>
+          <div className={styles.project}>
+            <img src={CurriculumDevices} alt="" />
+            <h3>Curriculum App</h3>
+            <p className={styles.description}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+{
+  /* <Card>
             <div className={styles.overlay}></div>
             <h3>Food Delivery App</h3>
             <Button
@@ -21,20 +45,5 @@ export default function Portfolio() {
               alt="Food Delivery On Github"
               name="Show On Github"
             ></Button>
-          </Card>
-          <Card>
-            <div className={styles.overlay}></div>
-            <h3>Curriculum App</h3>
-            <Button
-              url="https://github.com/Raquel-Almeida/curriculum"
-              alt="Curriculum On Github"
-              name="Show On Github"
-            ></Button>
-          </Card>
-        </div>
-        <img src={FoodDeliveryDevices} alt="" />
-        <img src={CurriculumDevices} alt="" />
-      </div>
-    </section>
-  );
+          </Card> */
 }
--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -4,8 +4,8 @@ import styles from "./Timeline.module.scss";
 
 export default function Timeline() {
   return (
-    <div className={styles.timeline} id="timeline">
-      <div className={styles.jobs}>
+    <div className={styles.timeline_container} id="timeline">
+      <div className={styles.timeline}>
         <h2>02. Timeline</h2>
         <div>A</div>
         <div>B</div>
