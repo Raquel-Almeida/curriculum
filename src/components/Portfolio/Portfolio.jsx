66e630f262c1da6586a0d4b24cc29c93e595c1a5
Raquel Almeida <87998797+Raquel-Almeida@users.noreply.github.com>
2022-03-08 21:02:11 +0000

    Portfolio Updated

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css/bundle";
 
 import styles from "./Portfolio.module.scss";
 import FoodDeliveryDevices from "../../assets/FoodDeliveryDevices.jpg";
 import CurriculumDevices from "../../assets/CurriculumDevices.jpg";
+import { FiGithub } from "react-icons/fi";
 
 export default function Portfolio() {
   return (
@@ -10,26 +14,76 @@ export default function Portfolio() {
       <div className={styles.portfolio}>
         <h2>03. Things I've Built</h2>
         <div className={styles.projects}>
-          <div className={styles.project}>
-            <img src={FoodDeliveryDevices} alt="" />
-            <h3>Food Delivery App</h3>
-            <p className={styles.description}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.{" "}
-            </p>
-          </div>
-          <div className={styles.project}>
-            <img src={CurriculumDevices} alt="" />
-            <h3>Curriculum App</h3>
-            <p className={styles.description}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.{" "}
-            </p>
-          </div>
+          <Swiper
+            spaceBetween={300}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className={styles.project}>
+                <a
+                  href="https://raquel-almeida.github.io/food-delivery"
+                  alt="Food Delivery App"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={FoodDeliveryDevices}
+                    alt="Food Delivery App Preview"
+                  />
+                </a>
+                <h3>Food Delivery App</h3>
+                <a
+                  className={styles.github_link}
+                  href="https://github.com/Raquel-Almeida/food-delivery"
+                  alt="Food Delivery App On Github"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FiGithub />
+                </a>
+                <p className={styles.description}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles.project}>
+                <a
+                  href="https://raquel-almeida.github.io/curriculum"
+                  alt="Curriculum App"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={CurriculumDevices} alt="Curriculum App Preview" />
+                </a>
+                <h3>Curriculum App</h3>
+                <a
+                  className={styles.github_link}
+                  href="https://github.com/Raquel-Almeida/curriculum"
+                  alt="Curriculum App On Github"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FiGithub />
+                </a>
+                <p className={styles.description}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </section>
