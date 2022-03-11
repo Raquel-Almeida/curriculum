751edfd8361881e739944a5492f116e1aad246d7
Raquel Almeida <87998797+Raquel-Almeida@users.noreply.github.com>
2022-03-11 18:57:17 +0000

    Timeline Updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,14 +14,12 @@ export default function About() {
         ></img>
         <div className={styles.about_me_container}>
           <p className={styles.job}>Front-End Web Developer</p>
-          <h1 className={styles.name}>
-            Hi, I'm Raquel Almeida. Nice to meet you.
-          </h1>
+          <h1>Hi, I'm Raquel Almeida. Nice to meet you.</h1>
           <p className={styles.about_me}>
             I'm extremely motivated to constantly improve my skills, overcome
             hurdles and grow professionally. I enjoy working in a team
             enviroment and I do well under pressure. In my spare time I love to
-            travell, discover new places and cultures, watch movies and series
+            travel, discover new places and cultures, watch movies and series
             and collect vinyl records.
           </p>
         </div>
--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -7,6 +7,44 @@ export default function Timeline() {
     <section className={styles.timeline_container} id="timeline">
       <div className={styles.timeline}>
         <h2>03. Where I've Worked</h2>
+        <div className={styles.card}>
+          <div className={styles.job}>
+            <h4>
+              Groundforce
+              <p>From Oct. 2019 - Oct. 2020</p>
+            </h4>
+            <p className={styles.job_description}>
+              <i>
+                <b>Role:</b> Check-in Agent and Gate Agent, at Lisbon Airport
+              </i>
+              At this job I had the amazing opportunity to deal with people from
+              all over the world and different cultures. I've learned to work
+              under tremendous pressure. Some of my main responsabilities were
+              to check passengers in, weighing and checking in their baggage,
+              assigning seats, issuing boarding passes and luggage labels,
+              monitoring jet way doors during boarding and disembarkation,
+              assisting in customer service duties and many other tasks.
+            </p>
+          </div>
+          <div className={styles.job}>
+            <h4>
+              Nova Ertek
+              <p>From Jun. 2018 - Oct. 2019</p>
+            </h4>
+            <p className={styles.job_description}>
+              <i>
+                <b>Role:</b> Check-in Agent and Gate Agent, at Lisbon Airport
+              </i>
+              At this job I had the amazing opportunity to deal with people from
+              all over the world and different cultures. I've learned to work
+              under tremendous pressure. Some of my main responsabilities were
+              to check passengers in, weighing and checking in their baggage,
+              assigning seats, issuing boarding passes and luggage labels,
+              monitoring jet way doors during boarding and disembarkation,
+              assisting in customer service duties and many other tasks.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
